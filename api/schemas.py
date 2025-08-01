66682e9d08c16ebebcf44cd223574df54fce58b4
Mbuvi2003 <115734723+Mbuvi2003@users.noreply.github.com>
2025-08-01 09:46:53 +0300

    Update schemas.py

--- api/schemas.py
+++ api/schemas.py
@@ -1,1 +1,33 @@
-#schemas
+# api/schemas.py
+
+from pydantic import BaseModel
+from typing import List
+
+# Request schema for Fit Analyzer
+class FitAnalyzerRequest(BaseModel):
+    resume_text: str
+    job_description: str
+
+# Response schema for Fit Analyzer
+class FitAnalyzerResponse(BaseModel):
+    fit_score: float
+    fit_label: str
+    missing_skills: List[str]
+    explanation: str
+
+# Request schema for Job Recommender
+class CareerRecommendationRequest(BaseModel):
+    resume_text: str
+    top_n: int = 5  # optional, default 5
+
+# Each recommended job field
+class CareerMatch(BaseModel):
+    role: str
+    description: str
+    score: float
+    matched_keywords: List[str]
+    explanation: str
+
+# Response schema for recommender
+class CareerRecommendationResponse(BaseModel):
+    recommendations: List[CareerMatch]
